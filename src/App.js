4ccdc1db96e6c11790fb5544d013d2d02b65fa84
Carlos Botero-Vargas <cbotero-vargas@outlook.com>
2021-07-16 11:22:05 -0400

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    // initialize the astronauts with an empty array
+    // to avoid running into null pointer exceptions
+    // after the first render, but before the componentDidMount() gets called
+    this.state = {
+      astronauts: [],
+      api: 'http://api.open-notify.org/astros.json',
+    };
+  }
+
+  // make sure to fetch from this React life cycle
+  componentDidMount() {
+    // use JavaScript's fetch API
+    fetch(this.state.api)
+      .then(res => res.json())
+      .then(data => {
+        // update the component's local state to the json object's people array
+        this.setState({
+          astronauts: data.people
+        });
+      });
+  } 
+
+  render() {
+    // create a list node to add to the DOM
+    const astronautList = this.state.astronauts.map((astronaut, index) => (
+      <li key={`astro-${index}`}>
+        <p>{astronaut.name} is flying in the {astronaut.craft} spacecraft</p>
+      </li>
+    ));
+
+    return (
+      <div className="App">
+        <header className="App-header">
+          <ol>
+            {astronautList}
+          </ol>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
